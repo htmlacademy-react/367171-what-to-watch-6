d5c8a4c716358736f33e5c4737e71ca8e7d93e5b
Yulia <juliam2007@mail.ru>
2021-02-07 16:30:32 +0300

    feat(movie-card): добавлен компонент превью карточки фильма

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import MainPage from "../main/main";
 
+const data = [];
+for (let i = 0; i < 20; i++) {
+  data.push({
+    key: i,
+    title: `Fantastic Beasts: The Crimes of Grindelwald ${i}`,
+    img: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  });
+}
+
 const App = () => {
 
   return (
-    <MainPage/>
+    <MainPage items={data}/>
   );
 };
 
