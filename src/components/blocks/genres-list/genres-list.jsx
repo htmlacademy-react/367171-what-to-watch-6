2932b2000b8cbb396eafce64b75f88cb03b58ce3
Yulia <juliam2007@mail.ru>
2021-02-10 13:43:51 +0300

    feat(components):  исправлены ошибки из консоли

--- src/components/blocks/genres-list/genres-list.jsx
+++ src/components/blocks/genres-list/genres-list.jsx
@@ -2,24 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 
-const GenreItem = ({key, label, selectedItem}) => {
-  return (
-    <li key={key} className={classnames(`catalog__genres-item`, {[`catalog__genres-item--active`]: selectedItem})}>
-      <a href="#" className="catalog__genres-link">{label}</a>
-    </li>
-  );
-};
-
-GenreItem.propTypes = {
-  key: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  selectedItem: PropTypes.boolean
-};
-
 const GenresList = ({items}) => {
   return (
     <ul className="catalog__genres-list">
-      {items.map(({name, label, selectedItem}) => <GenreItem label={label} key={name} selectedItem={selectedItem}/>)}
+      {items.map(({name, label, selectedItem}) => {
+        return (
+          <li key={name} className={classnames(`catalog__genres-item`, {[`catalog__genres-item--active`]: selectedItem})}>
+            <a href="#" className="catalog__genres-link">{label}</a>
+          </li>
+        );
+      })}
     </ul>
   );
 };
@@ -29,7 +21,7 @@ GenresList.propTypes = {
       PropTypes.shape({
         name: PropTypes.string.isRequired,
         label: PropTypes.string.isRequired,
-        selectedItem: PropTypes.boolean
+        selectedItem: PropTypes.bool
       })
   ).isRequired,
 };
