8c7c92fbbea790a71d14bbf4ac96660cfe0926dd
Yulia <juliam2007@mail.ru>
2021-02-19 16:24:30 +0300

    feat(movie-card): рефакторинг компонента add-review

--- src/components/blocks/add-review-form/add-review-form.jsx
+++ src/components/blocks/add-review-form/add-review-form.jsx
@@ -1,4 +1,31 @@
 import React, {useState} from "react";
+import PropTypes from "prop-types";
+
+const RATING = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const RatingStarsItem = ({item}) => {
+  return (
+    <>
+      <input className="rating__input" id={`star-${item}`} type="radio" name="rating" value={item}/>
+      <label className="rating__label" htmlFor={`star-${item}`}>Rating {item}</label>
+    </>
+  );
+};
+
+RatingStarsItem.propTypes = {item: PropTypes.number};
+
+
+const RatingStars = () => {
+  return (
+    <div className="rating">
+      <div className="rating__stars">
+        {RATING.map((item) => (<RatingStarsItem key={item} item={item}/>))}
+      </div>
+    </div>
+  );
+};
+
+RatingStars.propTypes = {array: PropTypes.arrayOf(PropTypes.number)};
 
 const AddReviewForm = () => {
 
@@ -6,7 +33,7 @@ const AddReviewForm = () => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-  }
+  };
 
   const handleFieldChange = (evt) => {
     const {value} = evt.target;
@@ -16,39 +43,7 @@ const AddReviewForm = () => {
   return (
     <div className="add-review">
       <form action="#" className="add-review__form" onSubmit={handleSubmit}>
-        <div className="rating">
-          <div className="rating__stars">
-            <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
-            <label className="rating__label" htmlFor="star-1">Rating 1</label>
-
-            <input className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
-            <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-            <input className="rating__input" id="star-3" type="radio" name="rating" value="3" checked/>
-            <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-            <input className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
-            <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-            <input className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
-            <label className="rating__label" htmlFor="star-5">Rating 5</label>
-
-            <input className="rating__input" id="star-6" type="radio" name="rating" value="6"/>
-            <label className="rating__label" htmlFor="star-6">Rating 6</label>
-
-            <input className="rating__input" id="star-7" type="radio" name="rating" value="7"/>
-            <label className="rating__label" htmlFor="star-7">Rating 7</label>
-
-            <input className="rating__input" id="star-8" type="radio" name="rating" value="8" checked/>
-            <label className="rating__label" htmlFor="star-8">Rating 8</label>
-
-            <input className="rating__input" id="star-9" type="radio" name="rating" value="9"/>
-            <label className="rating__label" htmlFor="star-9">Rating 9</label>
-
-            <input className="rating__input" id="star-10" type="radio" name="rating" value="10"/>
-            <label className="rating__label" htmlFor="star-10">Rating 10</label>
-          </div>
-        </div>
+        <RatingStars />
 
         <div className="add-review__text">
           <textarea onChange={handleFieldChange}
