784bcffb78bf103edb0e28a042ba15c6c8aca4b2
Yulia <juliam2007@mail.ru>
2021-02-23 20:21:20 +0300

    refactor(component): создан компонент Player, а старый переименован в PlayerPage

---
+++ src/components/pages/player-page/player-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainLayout from "../../layouts/main-layout/main-layout";
+import Player from "../../sections/player/player";
+
+const PlayerPage = () => {
+
+  return (
+    <MainLayout>
+      <Player/>
+    </MainLayout>
+  );
+};
+
+export default PlayerPage;
