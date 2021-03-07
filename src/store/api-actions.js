f35d21999c5a4e3e204e8dce24f715b381fd629f
Yulia <juliam2007@mail.ru>
2021-03-07 19:33:04 +0300

    feat(api): данные с бека адатированы под фронтенд

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,9 +1,20 @@
 import {loadMovies, requireAuthorization} from "./actions";
 import {AuthorizationStatus} from "../../../guess-melody-6/src/const";
 
+const transformMovie = (movie) => ({
+  posterImage: movie.poster_image,
+  previewImage: movie.preview_image,
+  backgroundImage: movie.background_image,
+  backgroundColor: movie.background_color,
+  isFavorite: movie.is_favorite,
+  videoLink: movie.video_link,
+  previewVideoLink: movie.preview_video_link
+});
+
 export const fetchMoviesList = () => (dispatch, _getState, api) => (
   api.get(`/films`)
-    .then(({data}) => dispatch(loadMovies(data)))
+    .then(({data}) => data.map((item) => transformMovie(item)))
+    .then((data) => dispatch(loadMovies(data)))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
