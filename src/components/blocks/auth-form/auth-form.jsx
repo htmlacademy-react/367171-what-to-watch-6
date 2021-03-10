fa2cc9784674c552e987d401c469f968366988d9
Yulia <juliam2007@mail.ru>
2021-03-10 14:18:09 +0300

    feat(auth): добавлена авторизация

--- src/components/blocks/auth-form/auth-form.jsx
+++ src/components/blocks/auth-form/auth-form.jsx
@@ -1,27 +1,66 @@
-import React from "react";
+import React, {useRef} from "react";
+import {useHistory} from "react-router-dom";
+import {RoutePath} from "../../constants/routes";
+import {connect} from "react-redux";
+import {login} from "../../../store/api-actions";
 
-const AuthForm = () => {
+const AuthForm = ({onSubmit}) => {
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  const history = useHistory();
+
+  useEffect(()=> {
+    if (loginRef.current) {
+      loginRef.current !==
+    }
+  });
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
   return (
-    <form action="#" className="sign-in__form">
+    <form action="#" className="sign-in__form" onSubmit={handleSubmit}>
 
       <div className="sign-in__fields">
         <div className="sign-in__field">
-          <input className="sign-in__input" type="email" placeholder="Email address" name="user-email"
+          <input ref={loginRef}
+            className="sign-in__input"
+            type="email"
+            placeholder="Email address"
+            name="user-email"
             id="user-email"/>
           <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
         </div>
         <div className="sign-in__field">
-          <input className="sign-in__input" type="password" placeholder="Password" name="user-password"
+          <input ref={passwordRef}
+            className="sign-in__input"
+            type="password"
+            placeholder="Password"
+            name="user-password"
             id="user-password"/>
           <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
         </div>
       </div>
 
       <div className="sign-in__submit">
-        <button className="sign-in__btn" type="submit">Sign in</button>
+        <button className="sign-in__btn" type="submit" onClick={() => history.push(RoutePath.ROOT)}>Sign in</button>
       </div>
     </form>
   );
 };
 
-export default AuthForm;
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  }
+});
+
+export {AuthForm};
+export default connect(null, mapDispatchToProps)(AuthForm);
