fef709fde224c25823407ad718e1651603786c12
Yulia <juliam2007@mail.ru>
2021-03-12 13:11:25 +0300

    feat(auth-form): настроена валидация полей в auth-form

--- src/components/blocks/auth-form/auth-form.jsx
+++ src/components/blocks/auth-form/auth-form.jsx
@@ -4,15 +4,29 @@ import {RoutePath} from "../../constants/routes";
 import {connect} from "react-redux";
 import {login} from "../../../store/api-actions";
 import PropTypes from "prop-types";
+import {useForm} from "react-hook-form";
+import classnames from "classnames";
 
 const AuthForm = ({onSubmit}) => {
   const loginRef = useRef();
   const passwordRef = useRef();
-
+  const {register, handleSubmit, errors} = useForm();
   const history = useHistory();
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
+  const userMailRef = (evt) => {
+    loginRef.current = evt;
+    register(evt, {
+      required: true,
+      pattern: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    });
+  };
+
+  const passWordRef = (evt) => {
+    passwordRef.current = evt;
+    register(evt, {required: true});
+  };
+
+  const onFormSubmit = () => {
     onSubmit({
       login: loginRef.current.value,
       password: passwordRef.current.value,
@@ -22,19 +36,23 @@ const AuthForm = ({onSubmit}) => {
 
   return (
     <form action="#" className="sign-in__form">
+      {errors.userEmail &&
+      (<div className="sign-in__message">
+        <p>Please enter a valid email address</p>
+      </div>)}
 
       <div className="sign-in__fields">
-        <div className="sign-in__field">
-          <input ref={loginRef}
+        <div className={classnames(`sign-in__fields`, {[`sign-in__field--error`]: errors.userEmail})}>
+          <input ref={userMailRef}
             className="sign-in__input"
             type="email"
             placeholder="Email address"
-            name="user-email"
+            name="userEmail"
             id="user-email"/>
           <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
         </div>
-        <div className="sign-in__field">
-          <input ref={passwordRef}
+        <div className={classnames(`sign-in__fields`, {[`sign-in__field--error`]: errors.password})}>
+          <input ref={passWordRef}
             className="sign-in__input"
             type="password"
             placeholder="Password"
@@ -45,7 +63,7 @@ const AuthForm = ({onSubmit}) => {
       </div>
 
       <div className="sign-in__submit">
-        <button className="sign-in__btn" type="submit" onClick={handleSubmit}>Sign in</button>
+        <button className="sign-in__btn" type="submit" onClick={handleSubmit(onFormSubmit)}>Sign in</button>
       </div>
     </form>
   );
